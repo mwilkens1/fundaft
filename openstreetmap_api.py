5563c58fc22cf47b98ec2690bbaf7c5c9f295e08
mwilkens1 <mathijn.wilkens@gmail.com>
2019-06-18 23:07:24 +0100

    more efficient openstreetmap query

--- openstreetmap_api.py
+++ openstreetmap_api.py
@@ -1,79 +1,78 @@
-# NOTE: cannot be run in Atom; throws an error for the parallel processing
 import pandas as pd
 import requests
 import json
 import numpy as np
+import random
 
 df_ads = pd.read_pickle('df_ads.pkl')
 
-overpass_url = "https://overpass.kumi.systems/api/interpreter"
+overpass_url = ["https://overpass.kumi.systems/api/interpreter","https://lz4.overpass-api.de/api/interpreter","https://z.overpass-api.de/api/interpreter"]
 
-def addmapinfo(type,radius=500):
+def addmapinfo(dict,radius=500):
 
-    data = []
+    df = []
 
-    # Build multiple queries in case one query has several types, e.g. pubs and bars
-    for i in type:
-        data_i = []
+    # Performing the query to the API for each row of data
+    for index, row in df_ads.iterrows():
 
-        for index, row in df_ads.iterrows():
-            lat = row["latitude"]
-            lon = row["longitude"]
+        data = []
 
-            overpass_query = """
-            [out:json];
-            (
-            node[""" + i + """](around:""" + str(radius) + """,""" + str(lat) + """,""" + str(lon) + """);
-            way[""" + i + """](around:""" + str(radius) + """,""" + str(lat) + """,""" + str(lon) + """);
-            relation[""" + i + """](around:""" + str(radius) + """,""" + str(lat) + """,""" + str(lon) + """);
-            );
-            out center;
-            """
-            response = requests.get(overpass_url, params={'data': overpass_query})
-            response_json = response.json()
+        lat = row["latitude"]
+        lon = row["longitude"]
 
-            data_i.append(len(response_json['elements']))
+        # Compiling the query to the API to account for multiple queries (e.g. pubs and bars at the same time)
+        string = ""
+        for i in dict.values():
 
-        data = list(np.sum([data,data_i],axis=0))
+            for j in i:
 
-    return(data)
+                string = string + """   node[""" + str(j).replace("'","") + """](around:""" + str(radius) + """,""" + str(lat) + """,""" + str(lon) + """);
+                way[""" + str(j).replace("'","") + """](around:""" + str(radius) + """,""" + str(lat) + """,""" + str(lon) + """);
+                relation[""" + str(j).replace("'","") + """](around:""" + str(radius) + """,""" + str(lat) + """,""" + str(lon) + """);
+                """
+
+        overpass_query = """
+        [out:json];
+        (
+        """ + string + """
+        );
+        out center;
+        """
+
+        response = requests.get(overpass_url[random.randint(0,2)], params={'data': overpass_query})
+        response_json = response.json()
+
+        # Counting the occurances of any of the values of a particular key
+        for key, value in dict.items():
+
+            count = 0
+
+            l = []
+
+            for j in value:
+
+                j = j.replace('"',"'").replace("=",": ")
+                l.append(j)
+
+            for e in response_json["elements"]:
+
+                if any(x in str(e["tags"]) for x in l):
+
+                    count += 1
+
+            data.append(count)
+
+        df.append(data)
+
+    df = pd.DataFrame(df, columns = dict.keys())
+
+    return(df)
+
 
 type_dict = {"pubs": ['"amenity"="pub"','"amenity"="bar"'], "caferestaurants": ['"amenity"="restaurant"','"amenity"="cafe"'], 'schools': ['"amenity"="school"'], 'stations': ['"railway"="platform"'], "platforms": ['"public_transport"="platform"'],'parks': ['"leisure"="park"'], 'churches': ['"amenity"="place_of_worship"'], 'health': ['"amenity"="clinic"','"amenity"="doctor"'], 'sports': ['"leisure"="pitch"','"leisure"="fitness_centre"','"leisure"="sports_centre"'], 'shops': ['"shop"']}
 
-#df_mapdata = pd.DataFrame()
+df_mapdata = addmapinfo(dict=type_dict)
 
-#for key,value in type_dict.items():
+df_ads_mapdata = pd.concat([df_ads,df_mapdata], axis=1)
 
-#    data = addmapinfo(type=value)
-
-#    df_mapdata = pd.concat([df_mapdata,pd.DataFrame({key: data})], axis=1)
-
-#df_ads_mapdata.to_pickle('df_ads_mapdata.pkl')    
-
-
-# Parallel processing with a worker per variable
-from multiprocessing import Pool
-
-# worker function
-def worker(value):
-    data = addmapinfo(type=value)
-    return data
-
-# overall job
-if __name__ == '__main__':
-
-    p = Pool()
-    data = p.map(worker, [value for key, value in type_dict.items()])
-
-    df_mapdata = pd.DataFrame()
-
-    i = 0
-    for key,value in type_dict.items():
-        df_mapdata = pd.concat([df_mapdata,pd.DataFrame({key: data[i]})], axis=1)
-        i += 1
-
-    df_ads_mapdata = pd.concat([df_ads,df_mapdata], axis=1)
-
-    df_ads_mapdata.to_pickle('df_ads_mapdata.pkl')
-
-    p.close()
+df_ads_mapdata.to_pickle('df_ads_mapdata_0_500.pkl')
