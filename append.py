4fe865b46126e067fd6e224b2fff6336df5060ed
mwilkens1 <mathijn.wilkens@gmail.com>
2019-06-27 06:03:57 +0100

    changed openstreetmap API code to reduce queries

---
+++ append.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df_ads_old = pd.read_pickle('df_ads_1606.pkl')
+
+df_ads_new = pd.read_pickle('df_ads.pkl')
+
+len(df_ads_old)
+len(df_ads_new)
+
+df_ads = df_ads_old.append(df_ads_new, ignore_index=True, sort=True)
+
+assert len(df_ads) == len(df_ads_old) + len(df_ads_new)
+
+df_ads.drop_duplicates()
+
+df_ads.to_pickle('df_ads.pkl')
--- openstreetmap_api.py
+++ openstreetmap_api.py
@@ -1,101 +1,89 @@
 import pandas as pd
 import requests
-
-df_ads = pd.read_pickle('df_ads.pkl')
+import geopy.distance
 
 overpass_url = "https://overpass.kumi.systems/api/interpreter"
 
-def addmapinfo(dict,radius=500, tries=5):
-
-    df = []
-
-    # Performing the query to the API for each row of data
-    for index, row in df_ads.iterrows():
-
-        data = []
-
-        lat = row["latitude"]
-        lon = row["longitude"]
-
-        # Compiling the query to the API to account for multiple queries (e.g. pubs and bars at the same time)
-        string = ""
-        for i in dict.values():
-
-            for j in i:
-
-                string = string + """   node[""" + str(j).replace("'","") + """](around:""" + str(radius) + """,""" + str(lat) + """,""" + str(lon) + """);
-                way[""" + str(j).replace("'","") + """](around:""" + str(radius) + """,""" + str(lat) + """,""" + str(lon) + """);
-                relation[""" + str(j).replace("'","") + """](around:""" + str(radius) + """,""" + str(lat) + """,""" + str(lon) + """);
-                """
-
-        overpass_query = """
-        [out:json];
-        (
-        """ + string + """
-        );
-        out center;
-        """
-
-        # Server seems to time out or refuse randomly.
-        for n in range(tries):
-
-            try:
-
-                response = requests.get(overpass_url, params={'data': overpass_query})
-
-            except:
-
-                print("Exception occured for row " + str(index) + ", ad_id " + row.ad_id)
-                response = None
-                continue
-
-            else:
-
-                break
-
-        if response == None:
-
-            continue
-
-        # if the html response is not 200, go to the next row in the data
-        if response.status_code != 200:
-
-            print("Response code " + str(response.status_code) + " for row " + str(index) + ", ad_id " + row.ad_id)
-            continue
-
-        response_json = response.json()
-
-        # Counting the occurances of any of the values of a particular key
-        for key, value in dict.items():
-
-            count = 0
-
-            l = []
-
-            for j in value:
-
-                j = j.replace('"',"'").replace("=",": ")
-                l.append(j)
-
-            for e in response_json["elements"]:
-
-                if any(x in str(e["tags"]) for x in l):
-
-                    count += 1
-
-            data.append(count)
-
-        df.append(data)
-
-    df = pd.DataFrame(df, columns = dict.keys())
-
-    return(df)
-
+# Get list of lists of amenities in 30km around the centre of Dublin
 
 type_dict = {"pubs": ['"amenity"="pub"','"amenity"="bar"'], "caferestaurants": ['"amenity"="restaurant"','"amenity"="cafe"'], 'schools': ['"amenity"="school"'], 'stations': ['"railway"="platform"'], "platforms": ['"public_transport"="platform"'],'parks': ['"leisure"="park"'], 'churches': ['"amenity"="place_of_worship"'], 'health': ['"amenity"="clinic"','"amenity"="doctor"'], 'sports': ['"leisure"="pitch"','"leisure"="fitness_centre"','"leisure"="sports_centre"'], 'shops': ['"shop"']}
 
-df_mapdata = addmapinfo(dict=type_dict)
+osm_data = {}
 
-df_ads_mapdata = pd.concat([df_ads,df_mapdata], axis=1)
+for key, value in type_dict.items():
+
+    string = ""
+    for i in value:
+
+        string = string + """   node[""" + str(i).replace("'","") + """](around:30000,53.346300,-6.263100);
+        way[""" + str(i).replace("'","") + """](around:30000,53.346300,-6.263100);
+        relation[""" + str(i).replace("'","") + """](around:30000,53.346300,-6.263100);
+        """
+
+    overpass_query = """
+    [out:json];
+    (
+    """ + string + """
+    );
+    out center;
+    """
+
+    response = requests.get(overpass_url, params={'data': overpass_query})
+
+    assert response.status_code == 200
+
+    response_json = response.json()
+
+    dict_key = {}
+
+    for k in ["id","lat","lon"]:
+
+        x = []
+        for e in response_json["elements"]:
+
+            if k in e:
+                x.append(e[k])
+            else:
+                if k in e["center"]:
+                    x.append(e["center"][k])
+
+        dict_key.update({k:x})
+
+        df_key = pd.DataFrame(dict_key)
+
+    osm_data.update({key:df_key})
+
+
+# Using list comprehensions, count the occurances of each type of amenity in a 0.5 mile radius from each property
+
+df_ads = pd.read_pickle('df_ads.pkl')
+
+def loop_over_ads(lat,lon):
+
+    def func(x, y):
+        out = geopy.distance.distance((x,y),(lat,lon)).km < 0.5
+        return(out)
+
+    dict_loop = {}
+    for key, value in osm_data.items():
+
+        z = sum([func(x, y) for x, y in zip(value['lat'], value['lon'])])
+        dict_loop.update({key: z})
+
+    return(dict_loop)
+
+result = pd.DataFrame([loop_over_ads(lat,lon) for lat, lon in zip(df_ads['latitude'],df_ads['longitude'])])
+
+df_ads_mapdata = pd.concat([df_ads,result],axis=1)
 
 df_ads_mapdata.to_pickle('df_ads_mapdata.pkl')
+
+osm_df = pd.DataFrame()
+
+for key, value in osm_data.items():
+
+    df = pd.DataFrame(value)
+    df["variable"] = key
+    osm_df = osm_df.append(df)
+
+osm_df.to_pickle('osm_data.pkl')
