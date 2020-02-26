df68a4352f94eb4c9b72015d12731f56b8c406dc
mwilkens1 <mathijn.wilkens@gmail.com>
2020-02-26 00:01:31 +0000

    scrape_daft changed to oop

--- scrape_daft.py
+++ scrape_daft.py
@@ -4,71 +4,109 @@ import re
 import pandas as pd
 import geopy.distance
 import os
-from datetime import datetime, date
+from datetime import datetime, date, time
 from shutil import copyfile
+import numpy as np
 
-
-#Get number of days since last crawl.
-date_last_crawl = datetime.utcfromtimestamp(os.path.getmtime("df_ads.pkl")).date()
-timedelta = date.today() - date_last_crawl
-days_since_last_crawl = timedelta.days
-
-#Create backup
-copyfile('df_ads.pkl', 'df_ads_' + date_last_crawl.strftime('%d-%m-%Y') + '.pkl')
-
-url = 'https://www.daft.ie/dublin-city/property-for-sale/?ad_type=sale&' \
-      'advanced=1&s%5Bdays_old%5D=' + \
-      str(days_since_last_crawl-1) + \
-      '&s%5Badvanced%5D=1&searchSource=sale'
-
-class crawl_daft(scrapy.Spider):
+class Daft_spider(scrapy.Spider):
     """Crawler for Daft.ie using scrapy spider"""
-
-    # Required to get access to daft
     custom_settings = {
-      'USER_AGENT': {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/' \
-                                   '537.36 (KHTML, like Gecko) Chrome/41.0.22' \
-                                   '28.0 Safari/537.36'},
-      'LOG_LEVEL': "INFO",
+        'USER_AGENT': {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/'
+                       '537.36 (KHTML, like Gecko) Chrome/41.0.22'
+                       '28.0 Safari/537.36'},
+        'LOG_LEVEL': "INFO",
     }
     name = 'daft'
-    start_urls = [url]
 
-    def parse(self,response):
+    def parse(self, response):
         """Extract the links to all ads"""
 
-        links = response.css('a.PropertyInformationCommonStyles__addressCopy--'\
+        links = response.css('a.PropertyInformationCommonStyles__addressCopy--'
                              'link::attr(href)').extract()
-        
+
         # Crawl into each of them and pass them the parse_ad function
         for link in links:
             yield response.follow(url = link, callback = self.parse_ad)
 
         # Go to the next page of ads
         for a in response.css('li.next_page > a::attr(href)'):
-            yield response.follow(a, callback=self.parse)
+            yield response.follow(a, callback = self.parse)
 
     def parse_ad(self, response):
         """Parses data from the ads and appends it to list ads"""
         # Extracts a javascript variable that contains all info on the page
-        ad_data = response.css('script:contains("trackingParam")::text').get()
-        ad_data = re.findall(r'(?<=trackingParam = ).*?(?=;)',ad_data)[0]
-        # Appending it to a list
-        ads.append(eval(ad_data))
+        ad_data=response.css('script:contains("trackingParam")::text').get()
+        ad_data=re.findall(r'(?<=trackingParam = ).*?(?=;)', ad_data)[0]
 
-ads = []
+        #self.ads.append(eval(ad_data))
 
-process = CrawlerProcess()
-process.crawl(crawl_daft)
-process.start()
+        with open('scrape.txt', 'a') as f:
+             f.write(ad_data + "\n")
+        self.log('Saved file %s' % 'scrape.txt')
 
-# Convert list of dictionary
-df_ads = pd.DataFrame(ads)
+class Crawl_daft:
+
+    def __init__(self):
+        self.ads= []
+        self.days_since_last_crawl=int()
+        self.date_last_crawl=time()
+        self.start_urls=[]
+        self.data = pd.DataFrame()
+
+    def create_url(self):
+        """Using the days since last crawl, creates the starting URL for
+        the crawler"""
+
+        self.start_urls='https://www.daft.ie/dublin-city/property-for-sale/?' \
+            'ad_type=sale&advanced=1&s%5Bdays_old%5D=' + \
+            str(self.days_since_last_crawl-1) + \
+            '&s%5Badvanced%5D=1&searchSource=sale'
+        print("URL: " + self.start_urls)
+
+    def count_days_since_last_crawl(self):
+        """Calculates the number of days since the last crawl on the basis
+        of the date when the df_ads.pkl file was last modified"""
+
+        self.date_last_crawl=datetime.utcfromtimestamp(
+            os.path.getmtime("df_ads.pkl")).date()
+        timedelta = date.today() - self.date_last_crawl
+        self.days_since_last_crawl = timedelta.days
+        print("Days since last crawl: ", self.days_since_last_crawl)
+
+    def backup(self):
+        """Creates backup of df_ads.pkl file"""
+
+        copyfile('df_ads.pkl', 'df_ads_' +
+                 self.date_last_crawl.strftime('%d-%m-%Y') + '.pkl')
+        print("Backup created: df_ads_" +
+              self.date_last_crawl.strftime('%d-%m-%Y') + '.pkl')
+
+    def crawl(self):
+        process = CrawlerProcess()
+        process.crawl(Daft_spider, start_urls=[self.start_urls]) 
+        process.start()
+        
+        data = open('scrape.txt', 'r')
+        with data as f:
+            lines = f.read().splitlines()
+        data.close()
+        os.remove('scrape.txt')
+        
+        ads = []
+        for i in lines:
+            ads.append(eval(i.replace("null", "np.nan")))
+
+        self.data = pd.DataFrame(ads)
+
+test = Crawl_daft()
+test.count_days_since_last_crawl()
+test.create_url()
+test.backup()
+test.crawl()
+test.data.to_pickle('df_ads.pkl')
 
 # Calculate distance to centre
 hapenny = (53.346300, -6.263100)
 df_ads["dist_to_centre"] = df_ads.apply(lambda x: geopy.distance.distance(
                                                     (x.latitude,x.longitude),
                                                     hapenny).km, axis=1)
-
-df_ads.to_pickle('df_ads.pkl')
