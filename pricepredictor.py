31a14ceedfda14be62f0a5ffada085d5de11b505
mwilkens1 <mathijn.wilkens@gmail.com>
2020-11-03 15:21:47 +0100

    fix 2

--- pricepredictor.py
+++ pricepredictor.py
@@ -31,9 +31,9 @@ class PricePredictor():
         #Use selenium to bypass cookiewall
         chrome_options = webdriver.ChromeOptions()
         
-        # chrome_options.add_argument('--disable-gpu')
-        # chrome_options.add_argument('--no-sandbox')
-        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument("--headless")
         # driver = webdriver.Chrome(options=chrome_options)
 
         #Only for heroku
